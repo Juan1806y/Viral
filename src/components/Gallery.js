a6771ac46480e3b983974ae563e27b3bc2b6e6f0
LindaValentinaBohorquez <lindavalentinabh@ufps.edu.co>
2023-05-20 17:08:48 -0500

    Se centra Gallery componente en page Proyectos

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,22 +1,24 @@
-import React from "react"
-import "normalize.css"
-import "../assets/css/main.css"
-import Image1 from '../assets/img/Imagen1.jpg'
-import Image2 from '../assets/img/Imagen2.jpg'
-import Image3 from '../assets/img/Imagen3.png'
-import Image4 from '../assets/img/Imagen4.jpg'
-import Image5 from '../assets/img/Imagen5.jpg'
+import React from "react";
+import "normalize.css";
+import "../assets/css/main.css";
+import Image1 from "../assets/img/Imagen1.jpg";
+import Image2 from "../assets/img/Imagen2.jpg";
+import Image3 from "../assets/img/Imagen3.png";
+import Image4 from "../assets/img/Imagen4.jpg";
+import Image5 from "../assets/img/Imagen5.jpg";
 
 const Gallery = () => {
-    return (
-        <section className="gallery-body">
-            <img className="galley-image" src={Image1} alt="img1" />
-            <img className="galley-image" src={Image2} alt="img2" />
-            <img className="galley-image" src={Image3} alt="img3" />
-            <img className="galley-image" src={Image4} alt="img4" />
-            <img className="galley-image" src={Image5} alt="img5" />
-        </section>
-    )
-}
+  return (
+    <div className="gallery-container">
+      <section className="gallery-body">
+        <img className="galley-image" src={Image1} alt="img1" />
+        <img className="galley-image" src={Image2} alt="img2" />
+        <img className="galley-image" src={Image3} alt="img3" />
+        <img className="galley-image" src={Image4} alt="img4" />
+        <img className="galley-image" src={Image5} alt="img5" />
+      </section>
+    </div>
+  );
+};
 
-export default Gallery
+export default Gallery;
--- src/pages/proyectos.js
+++ src/pages/proyectos.js
@@ -2,43 +2,43 @@ import React from "react";
 import Layout from "../components/Layout";
 import Gallery from "../components/Gallery";
 import ArticleGame from "../components/ArticleGame";
-import Image1 from '../assets/img/Imagen1.jpg'
-import Image2 from '../assets/img/Imagen2.jpg'
-import Image3 from '../assets/img/Imagen3.png'
-import Image4 from '../assets/img/Imagen4.jpg'
-import Image5 from '../assets/img/Imagen5.jpg'
-
-
+import Image1 from "../assets/img/Imagen1.jpg";
+import Image2 from "../assets/img/Imagen2.jpg";
+import Image3 from "../assets/img/Imagen3.png";
+import Image4 from "../assets/img/Imagen4.jpg";
+import Image5 from "../assets/img/Imagen5.jpg";
 
 const Proyectos = () => {
   return (
     <Layout>
-      <Gallery />
-      <ArticleGame
-        src={Image1}
-        title={"Cap Trop"}
-        text={"La descripcion del videojuego"}
-      />
-      <ArticleGame
-        src={Image2}
-        title={"RECYCLOMAN"}
-        text={"La descripcion del videojuego"}
-      />
-      <ArticleGame
-        src={Image3}
-        title={"Fractio"}
-        text={"La descripcion del videojuego"}
-      />
-      <ArticleGame
-        src={Image4}
-        title={"Viral Space"}
-        text={"La descripcion del videojuego"}
-      />
-      <ArticleGame
-        src={Image5}
-        title={"FunMathS"}
-        text={"La descripcion del videojuego"}
-      />
+      <div className="page">
+        <Gallery />
+        <ArticleGame
+          src={Image1}
+          title={"Cap Trop"}
+          text={"La descripcion del videojuego"}
+        />
+        <ArticleGame
+          src={Image2}
+          title={"RECYCLOMAN"}
+          text={"La descripcion del videojuego"}
+        />
+        <ArticleGame
+          src={Image3}
+          title={"Fractio"}
+          text={"La descripcion del videojuego"}
+        />
+        <ArticleGame
+          src={Image4}
+          title={"Viral Space"}
+          text={"La descripcion del videojuego"}
+        />
+        <ArticleGame
+          src={Image5}
+          title={"FunMathS"}
+          text={"La descripcion del videojuego"}
+        />
+      </div>
     </Layout>
   );
 };
