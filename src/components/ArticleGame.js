9970ea336b2aa934ff80260dd8e612ffab53fed9
LindaValentinaBohorquez <lindavalentinabh@ufps.edu.co>
2023-05-20 18:12:14 -0500

    Se hace redirección basica - por completar con graphql

--- src/components/ArticleGame.js
+++ src/components/ArticleGame.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "normalize.css";
 import "../assets/css/main.css";
+import { Link } from "gatsby";
 
 const ArticleGame = ({ src, title, text }) => {
   return (
     <div className="article-game-card">
-      <img src={src} alt="photo gamen" className="article-game-img" />
+      <Link to="/juego1">
+        <img src={src} alt="photo gamen" className="article-game-img" />
+      </Link>
       <div className="article-game-container">
         <h4>
           <b>{title}</b>
