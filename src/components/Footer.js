d769a074ad84bc8dd1d30553e9cc2e2aac817c3c
Juan1806y <juandavidorca@ufps.edu.co>
2023-06-12 21:07:46 -0500

    Versión Final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='page-footer'>
-        <p>&copy; {new Date().getFullYear()} <span> VIRAL. </span> Hecho con {""}
+        <p>&copy; {new Date().getFullYear()} <a href='/'> VIRAL. </a> Hecho con {""}
           <a href='https://www.gatsbyjs.com/'>Gatsby</a>
         </p>
     </footer>
